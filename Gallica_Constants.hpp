04b86ed4f78487ba5b5dc25bcacd5a0fa849b7ce
GallicaCH <cal_.223rem@hotmail.fr>
2013-07-16 19:47:40 -0400

    Create Gallica_Constants.hpp

---
+++ Gallica_Constants.hpp
@@ -0,0 +1,29 @@
+////////////////////////////////////////////////////////////////////////////////
+//        FILE : Gallica_Constants.hpp
+//      AUTHOR : Charles Hosson
+//        DATE :   Creation : July 15 2013
+//               Last Entry : July 16 2013
+// DESCRIPTION : Contains all the constants used by the Gallica program.
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+using namespace std;
+
+
+#include <string>
+
+
+
+
+const    int SCREEN_WIDTH = 1200;
+const    int SCREEN_HEIGHT = 700;
+const    int SCREEN_BPP = 32;
+const double SCREEN_FRAMERATE = 40.0;
+
+const int PANEL_HEIGHT = 100;
+
+const int LEVEL_WIDTH = 2000;
+const int LEVEL_HEIGHT = 2000;
+
+
